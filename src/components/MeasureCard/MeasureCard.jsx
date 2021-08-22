f86f47d4e2ee57335e3d90612445fbf0c7b5ce96
Michael <mdesormeaux96@gmail.com>
2021-08-22 15:21:03 -0500

    feat: added api ingestion for measurements

---
+++ src/components/MeasureCard/MeasureCard.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Segment } from "semantic-ui-react";
+
+export const MeasureCard = ({ measurements }) => {
+  const bc = measurements.filter((x) => x.parameter === "bc")[0];
+  const ch4 = measurements.filter((x) => x.parameter === "ch4")[0];
+  const co = measurements.filter((x) => x.parameter === "co")[0];
+  const co2 = measurements.filter((x) => x.parameter === "co2")[0];
+  const no = measurements.filter((x) => x.parameter === "no")[0];
+  const nox = measurements.filter((x) => x.parameter === "nox")[0];
+  const no2 = measurements.filter((x) => x.parameter === "no2")[0];
+  const o3 = measurements.filter((x) => x.parameter === "o3")[0];
+  const pm1 = measurements.filter((x) => x.parameter === "pm1")[0];
+  const pm25 = measurements.filter((x) => x.parameter === "pm25")[0];
+  const pm10 = measurements.filter((x) => x.parameter === "pm10")[0];
+  const so2 = measurements.filter((x) => x.parameter === "so2")[0];
+
+  const measureArray = [
+    bc,
+    ch4,
+    co,
+    co2,
+    no,
+    nox,
+    no2,
+    o3,
+    pm1,
+    pm25,
+    pm10,
+    so2,
+  ];
+
+  const filteredArray = measureArray.filter((index) => index !== undefined);
+
+  console.log(measureArray);
+
+  return (
+    <Segment color="brown">
+      Parameters:
+      <br />
+      {filteredArray.map((x) => (
+        <p>{x.parameter}</p>
+      ))}
+    </Segment>
+  );
+};
